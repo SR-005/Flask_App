9d8ec5a40469730b49468de5e69051319c506a0a
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-03 13:29:24 +0530

    update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #index is the main page
 @app.route("/")
 def index():
-    return "Hello Everyone"
+    return render_template("index.html")
 
 if __name__ in "__main__":
     app.run(debug=True) 
