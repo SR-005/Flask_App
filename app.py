0e395d4e1b3ac4a30b8213b1a7e0c56676487bfb
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-03 13:25:03 +0530

    update

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+#index is the main page
+@app.route("/")
+def index():
+    return "Hello Everyone"
+
+if __name__ in "__main__":
+    app.run(debug=True) 
